682721b8ce42b867b3150912551747badcb14505
Andrii Pyroh <andrewpyrih@gmail.com>
2024-05-15 13:56:59 +0300

    add flights page, flight nav bar item, add flight_service url

--- src/constants/pagesURLs.js
+++ src/constants/pagesURLs.js
@@ -2,9 +2,10 @@ import * as pages from './pages';
 import config from 'config';
 
 const result = {
-  [pages.defaultPage]: `${config.UI_URL_PREFIX}/${pages.defaultPage}`,
-  [pages.login]: `${config.UI_URL_PREFIX}/${pages.login}`,
-  [pages.secretPage]: `${config.UI_URL_PREFIX}/${pages.secretPage}`,
+    [pages.defaultPage]: `${config.UI_URL_PREFIX}/${pages.defaultPage}`,
+    [pages.login]: `${config.UI_URL_PREFIX}/${pages.login}`,
+    [pages.secretPage]: `${config.UI_URL_PREFIX}/${pages.secretPage}`,
+    [pages.flights]: `${config.UI_URL_PREFIX}/${pages.flights}`,
 };
 
 export default result;
